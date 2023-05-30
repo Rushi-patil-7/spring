097b6777eb36f8ef7169b5d0df84fee7e667323e
Rushi-patil-7 <rpatilofficial7@gmail.com>
2023-05-31 01:20:10 +0530

    commit for autowiring using XML

---
+++ springcore/src/main/java/com/springcore/autowiring/Company.java
@@ -0,0 +1,21 @@
+package com.springcore.autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Company {
+	public static void main(String[] args) {
+
+	ApplicationContext context =new	ClassPathXmlApplicationContext("com/springcore/autowiring/autoconfig.xml");
+	Car car1 =	(Car) context.getBean("cardetails1");
+
+	Car car2 =	(Car) context.getBean("cardetails2");
+
+	Car car3 =	(Car) context.getBean("cardetails3");
+
+	System.out.println(car1);
+	System.out.println(car2);
+	System.out.println(car3);
+		
+	}
+}
