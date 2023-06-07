7229e6a6243a24ed3923d885ceeae0654534059e
Rushi-patil-7 <rpatilofficial7@gmail.com>
2023-06-08 00:21:19 +0530

    commit for change in spring-mvc -> spring-servle.xml

--- springmvc/src/main/java/com/spring/mvc/hibernateorm/dao/UserDAO.java
+++ springmvc/src/main/java/com/spring/mvc/hibernateorm/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package com.spring.mvc.hibernateorm.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import com.spring.mvc.controller.User;
 public class UserDAO {
 
 	@Autowired
+	@Qualifier("hibernateTemplate")
 	private HibernateTemplate hibernateTemplate;
 	
 	@Transactional
